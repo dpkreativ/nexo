b3db4ccc9e3402111e82ab28112b9eeb0e0d82cf
Divine Orji <dpkreativ@gmail.com>
2021-11-05 15:10:44 +0100

    dashboard component

---
+++ components/DashboardPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./General/Header";
+
+const DashboardPage = () => {
+  return (
+    <>
+      <Header />
+      <main>Hello Divine, this is the dashboard</main>
+    </>
+  );
+};
+
+export default DashboardPage;
