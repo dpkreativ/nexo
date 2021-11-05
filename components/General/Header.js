f442bf989d48cc2df59051d5350a4e6ffa108631
Divine Orji <dpkreativ@gmail.com>
2021-11-05 11:56:41 +0100

    create header component

---
+++ components/General/Header.js
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import Button from "./Button";
+import { HamburgerIcon, Logo } from "./Icons";
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="p-5 w-full max-w-6xl mx-auto flex justify-between item-center">
+        <div className="my-auto">
+          <Link href="/" passHref>
+            <a className="w-20 h-5 block">
+              <Logo />
+            </a>
+          </Link>
+        </div>
+        <div className="lg:hidden my-auto">
+          <Link href="/" passHref>
+            <a className="w-8 h-5 block">
+              <HamburgerIcon />
+            </a>
+          </Link>
+        </div>
+        <div className="lg:flex lg:space-x-5 hidden">
+          <div>
+            <Button className="bg-indigo-500 text-white hover:bg-indigo-600">
+              Sign up
+            </Button>
+          </div>
+          <div>
+            <Button className="hover:bg-indigo-100 text-indigo-500">
+              Sign in
+            </Button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
