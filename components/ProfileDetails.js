495beef4f2b7429680c50f727d77232e5c2bbd08
Divine Orji <dpkreativ@gmail.com>
2021-11-05 10:45:00 +0100

    create profile details component

---
+++ components/ProfileDetails.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const ProfileDetails = () => {
+  return (
+    <>
+      <Head>
+        <title>User profile</title>
+      </Head>
+<main>
+  
+</main>
+    </>
+  );
+};
+
+export default ProfileDetails;
