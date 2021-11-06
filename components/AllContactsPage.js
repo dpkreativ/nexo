6be64d7dbb9e545dfeb9eb85dd4b13f1136e4f8a
Divine Orji <dpkreativ@gmail.com>
2021-11-06 10:01:48 +0100

    build contact page

--- components/AllContactsPage.js
+++ components/AllContactsPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ContactCard from "./General/Cards/ContactCard";
 import Sidebar from "./General/Sidebar";
 
 const AllContactsPage = () => {
@@ -8,7 +9,46 @@ const AllContactsPage = () => {
         <Sidebar />
       </div>
       <main className="p-5 w-full bg-gray-50">
-        <h1>All Contacts</h1>
+        <div className="mb-10">
+          <h1 className="font-bold text-2xl text-indigo-500">2 Contacts</h1>
+        </div>
+        <div>
+          <div className="grid lg:grid-cols-6 gap-6">
+            <div>
+              <ContactCard />
+            </div>
+            <div>
+              <ContactCard />
+            </div>
+            <div>
+              <ContactCard />
+            </div>
+            <div>
+              <ContactCard />
+            </div>
+            <div>
+              <ContactCard />
+            </div>
+            <div>
+              <ContactCard />
+            </div>
+            <div>
+              <ContactCard />
+            </div>
+            <div>
+              <ContactCard />
+            </div>
+            <div>
+              <ContactCard />
+            </div>
+            <div>
+              <ContactCard />
+            </div>
+            <div>
+              <ContactCard />
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
---
+++ components/General/Cards/ContactCard.js
@@ -0,0 +1,28 @@
+import { PhoneIcon, ProfileIcon } from "../Icons";
+
+const ContactCard = () => {
+  return (
+    <div className="rounded-3xl p-5 border border-indigo-500 shadow-md w-40">
+      <div className="w-full flex justify-center mb-2">
+        <div className="w-16 h-16 rounded-full bg-gray-400">
+          <div className="w-full h-full flex items-center justify-center text-gray-200">
+            <ProfileIcon />
+          </div>
+        </div>
+      </div>
+      <div className="text-center mb-2">
+        <h2 className="font-bold text-lg">Lil Wayne</h2>
+        <p>Chicken</p>
+      </div>
+      <div className="w-full flex justify-center">
+        <div className="bg-indigo-300 w-10 h-10 rounded-full">
+          <div className="h-full w-full flex items-center justify-center text-indigo-200">
+            <PhoneIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactCard;
